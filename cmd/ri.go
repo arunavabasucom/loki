82b8c83b66405c066c02eb1ffb21177afb3d424a
Arunava Basu <arunavabasudev@gmail.com>
2024-05-16 20:56:51 +0530

    feat: adding spinner for reverse ip lookup

--- cmd/ri.go
+++ cmd/ri.go
@@ -3,7 +3,9 @@ package cmd
 import (
 	"fmt"
 	"net"
+	"time"
 
+	"github.com/briandowns/spinner"
 	"github.com/spf13/cobra"
 )
 
@@ -13,17 +15,17 @@ var riCmd = &cobra.Command{
 	Long:  `Reverse IP lookup is a technique that allows you to find all the domains hosted on a given IP address.`,
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		// s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
-		// s.Suffix = " looking for ips"
-		// s.Start()
-		// time.Sleep(1 * time.Second)
+		s := spinner.New(spinner.CharSets[11], 100*time.Millisecond)
+		s.Suffix = " looking for ips"
+		s.Start()
+		time.Sleep(1 * time.Second)
 		ip := args[0]
 		domains, err := net.LookupAddr(ip)
 		if err != nil {
 			fmt.Println("Error:", err)
 			return
 		}
-		// s.Stop();
+		s.Stop()
 
 		fmt.Printf("Domain(s) associated with IP %s:\n", ip)
 		for _, domain := range domains {
